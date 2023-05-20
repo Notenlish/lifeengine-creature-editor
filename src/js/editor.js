1a4fcd2665d7a3c9fcbe8ff1b206bf26a170b026
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-05-21 01:11:48 +0300

    fix: #9 - zooming in editor
    Also moved some constants to another file and got a rid of simple undeclared variable bug

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,306 +1,318 @@
-const canvas = document.querySelector("canvas");
-canvas.oncontextmenu = function(event) {
-    event.preventDefault();  // disable the right click context menu
-    event.stopPropagation();
-}
+import {
+  canvas,
+  canvasContainer,
+  ctx,
+  exportBtn,
+  importBtn,
+  resizeBtn,
+  nameInput,
+  foodInput,
+  directionInput,
+  mutationInput,
+  canvasWidthInput,
+  canvasHeightInput,
+  cellSizeInput,
+  cellNames,
+  colors,
+} from "./variables.js";
 
-const canvasContainer = document.querySelector(".canvas-container");
-const ctx = canvas.getContext("2d");
-const exportBtn = document.querySelector("#jsonexport");
-const importBtn = document.querySelector("#jsonimport");
-const resizeBtn = document.querySelector("#canvas-resize");
-
-const nameInput = document.querySelector("#org-name");
-const foodInput = document.querySelector("#org-food");
-const directionInput = document.querySelector("#org-direction");
-const mutationInput = document.querySelector("#org-mutation");
-const canvasWidthInput = document.querySelector("#canvas-width");
-const canvasHeightInput = document.querySelector("#canvas-height");
+canvas.oncontextmenu = function (event) {
+  event.preventDefault(); // disable the right click context menu
+  event.stopPropagation();
+};
 
 let cameraX = 0;
 let cameraY = 0;
 let cellSize = 50;
 let holding = false;
 let currentCellType = "producer";
-
 let halfGridWidth;
 let halfGridHeight;
+let zoom = 2;
 
-let topcenter = canvasContainer.getBoundingClientRect().height / 2 - canvas.getBoundingClientRect().height / 2;
-let leftcenter = canvasContainer.getBoundingClientRect().width / 2 - canvas.getBoundingClientRect().width / 2;
+let topcenter =
+  canvasContainer.getBoundingClientRect().height / 2 -
+  canvas.getBoundingClientRect().height / 2;
+let leftcenter =
+  canvasContainer.getBoundingClientRect().width / 2 -
+  canvas.getBoundingClientRect().width / 2;
 canvas.style.top = `${topcenter}px`;
 canvas.style.left = `${leftcenter}px`;
 cameraX = -leftcenter;
 cameraY = topcenter;
 
-const cellNames = {
-    producer: "green",
-    mouth: "orange",
-    killer: "red",
-    mover: "blue",
-    eye: "grey",
-    armor: "purple",
-};
-
-const colors = {
-    orange: "#DEB14D",
-    green: "#15DE59",
-    red: "#F82380",
-    grey: "#B7C1EA",
-    purple: "#7230DB",
-    blue: "#60D4FF",
-    black: "#222",
-    gray: "#333",
-};
-
 let organism = {
-    c: 7,   // The Life Engine Parameters
-    r: 7,   // The Life Engine Parameter
-    lifetime: 0,
-    food_collected: 0,
-    living: true,
-    direction: 2,
-    rotation: 0,
-    can_rotate: true,
-    move_count: 0,
-    move_range: 4,
-    ignore_brain_for: 0,
-    mutability: 1,
-    damage: 0,
-    anatomy: {
-        birth_distance: 50,
-        is_producer: true,
-        is_mover: false,
-        has_eyes: true,
-        cells: [],
-    },
-    species_name: "",
-    flip,
+  c: 7, // The Life Engine Parameters
+  r: 7, // The Life Engine Parameter
+  lifetime: 0,
+  food_collected: 0,
+  living: true,
+  direction: 2,
+  rotation: 0,
+  can_rotate: true,
+  move_count: 0,
+  move_range: 4,
+  ignore_brain_for: 0,
+  mutability: 1,
+  damage: 0,
+  anatomy: {
+    birth_distance: 50,
+    is_producer: true,
+    is_mover: false,
+    has_eyes: true,
+    cells: [],
+  },
+  species_name: "",
+  flip,
 };
 
 function flip(org) {
-    for (let i = 0; i < org.anatomy.cells.length; i++) {
-        [org.anatomy.cells[i].loc_col, org.anatomy.cells[i].loc_row] = [
-            org.anatomy.cells[i].loc_row,
-            org.anatomy.cells[i].loc_col,
-            ];
-    }
-    return org;
-};
-
-resizeBtn.addEventListener("click", (event) => resizeCanvas(event));
-
-function resizeCanvas(event) {
-    event.preventDefault(); // Prevents the default form submission behavior
-    canvas.width = canvasWidthInput.value * cellSize * 2 + cellSize;
-    canvas.height = canvasHeightInput.value * cellSize * 2 + cellSize;
-    halfGridWidth = Math.round(canvas.width / 4 / cellSize);
-    halfGridHeight = Math.round(canvas.height / 4 / cellSize);
-    drawCells();
-    updateGraph();
+  for (let i = 0; i < org.anatomy.cells.length; i++) {
+    [org.anatomy.cells[i].loc_col, org.anatomy.cells[i].loc_row] = [
+      org.anatomy.cells[i].loc_row,
+      org.anatomy.cells[i].loc_col,
+    ];
+  }
+  return org;
 }
 
+function ensureOdd(num) {
+  if (num % 2 === 0) {
+    return num + 1;
+  } else {
+    return num;
+  }
+}
+
+resizeBtn.addEventListener("click", resizeCanvas);
+function resizeCanvas(event) {
+  event.preventDefault(); // Prevents page refresh
+  cellSize = parseInt(cellSizeInput.value);
+  canvasWidthInput.value = ensureOdd(parseInt(canvasWidthInput.value));
+  canvasHeightInput.value = ensureOdd(parseInt(canvasHeightInput.value));
+  canvas.width = canvasWidthInput.value * cellSize * 2 + cellSize;
+  canvas.height = canvasHeightInput.value * cellSize * 2 + cellSize;
+  halfGridWidth = Math.round(canvas.width / 4 / cellSize);
+  halfGridHeight = Math.round(canvas.height / 4 / cellSize);
+  canvas.style.transform = `scale(${zoom})`;
+  drawCells();
+  updateGraph();
+}
+
+canvasContainer.addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    zoom += Math.max(event.deltaY * -0.06, 0.06);
+  } else if (event.deltaY < 0) {
+    zoom += Math.min(event.deltaY * -0.06, -0.06);
+  }
+  zoom = Math.min(Math.max(0.125, zoom), 4);
+  canvas.style.transform = `scale(${zoom})`;
+});
+
 function updateOrganism() {
-    organism.species_name = nameInput.value;
-    organism.food_collected = parseInt(foodInput.value);
-    organism.direction = parseInt(directionInput.value);
-    organism.mutability = parseInt(mutationInput.value);
+  organism.species_name = nameInput.value;
+  organism.food_collected = parseInt(foodInput.value);
+  organism.direction = parseInt(directionInput.value);
+  organism.mutability = parseInt(mutationInput.value);
 }
 
 exportBtn.addEventListener("click", (event) => {
-    updateOrganism();
-    let organismToExport = JSON.parse(JSON.stringify(organism)); // deep copy
-    organismToExport = flip(organismToExport);
-    let dataStr = JSON.stringify(organismToExport);
-    let dataUri =
-        "data:application/json;charset=utf-8," + encodeURIComponent(dataStr);
-    let exportFileDefaultName;
-    if (nameInput.value === "") {
-        exportFileDefaultName = "organism.json";
-    } else {
-        exportFileDefaultName = `${nameInput.value}.json`;
-    }
-    exportBtn.setAttribute("href", dataUri);
-    exportBtn.setAttribute("download", exportFileDefaultName);
+  updateOrganism();
+  let organismToExport = JSON.parse(JSON.stringify(organism)); // deep copy
+  organismToExport = flip(organismToExport);
+  let dataStr = JSON.stringify(organismToExport);
+  let dataUri =
+    "data:application/json;charset=utf-8," + encodeURIComponent(dataStr);
+  let exportFileDefaultName;
+  if (nameInput.value === "") {
+    exportFileDefaultName = "organism.json";
+  } else {
+    exportFileDefaultName = `${nameInput.value}.json`;
+  }
+  exportBtn.setAttribute("href", dataUri);
+  exportBtn.setAttribute("download", exportFileDefaultName);
 });
 
 let form = document.querySelector("#upload");
 let file = document.querySelector("#file");
-// form.addEventListener("submit", importJson);
 
 importBtn.addEventListener("click", (event) => {
-    file.click(); // Simulate a click on the file input button
-    // Make the file submit the form
-    file.addEventListener("change", importJson);
+  file.click(); // Simulate a click on the file input button
+  file.addEventListener("change", importJson); // Make the file submit the form
 });
 
 function parseFile(event) {
-    let str = event.target.result;
-    let json = JSON.parse(str);
-    organism = json;
-    organism = flip(organism);
-    
-    nameInput.value = organism.species_name;
-    foodInput.value = organism.food_collected;
-    directionInput.value = organism.direction;
-    mutationInput.value = organism.mutability;
-    
-    drawCells();
-    updateGraph();
+  let str = event.target.result;
+  let json = JSON.parse(str);
+  organism = json;
+  organism = flip(organism);
+
+  nameInput.value = organism.species_name;
+  foodInput.value = organism.food_collected;
+  directionInput.value = organism.direction;
+  mutationInput.value = organism.mutability;
+
+  drawCells();
+  updateGraph();
 }
 
 function importJson(event) {
-    event.preventDefault();
-    if (!file.value.length) return;
-    let reader = new FileReader();
-    reader.onload = parseFile;
-    reader.readAsText(file.files[0]);
+  event.preventDefault();
+  if (!file.value.length) return;
+  let reader = new FileReader();
+  reader.onload = parseFile;
+  reader.readAsText(file.files[0]);
 }
 
 let dragstartX;
 let dragstartY;
 canvasContainer.addEventListener("mousedown", (event) => {
-    if (event.button != 1) {
-        return;
-    }
-    dragstartX = event.clientX;
-    dragstartY = event.clientY;
-    document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener("mouseup", onMouseUp);
+  if (event.button != 1) {
+    return;
+  }
+  dragstartX = event.clientX;
+  dragstartY = event.clientY;
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
 });
 
 let cellButtons = document.getElementsByClassName("editor-cellbutton");
 for (let i = 0; i < cellButtons.length; i++) {
-    let cellButton = cellButtons[i];
-    cellButton.addEventListener("mousedown", (event) => {
-        if (event.button != 0) {
-            return;
-        }
-        for (let i = 0; i < cellButtons.length; i++) {
-            let otherCell = cellButtons[i];
-            delete otherCell.dataset.active;
-        }
-        cellButton.dataset.active = "";
-        currentCellType = cellButton.dataset.cell;
-    });
+  let cellButton = cellButtons[i];
+  cellButton.addEventListener("mousedown", (event) => {
+    if (event.button != 0) {
+      return;
+    }
+    for (let i = 0; i < cellButtons.length; i++) {
+      let otherCell = cellButtons[i];
+      delete otherCell.dataset.active;
+    }
+    cellButton.dataset.active = "";
+    currentCellType = cellButton.dataset.cell;
+  });
 }
 
 function onMouseMove(event) {
-    canvas.style.left = "";
-    let draggedX = event.clientX - dragstartX;
-    let draggedY = event.clientY - dragstartY;
-    dragstartX = event.clientX;
-    dragstartY = event.clientY;
-    cameraX -= draggedX;
-    cameraY += draggedY;
-    canvas.style.top = `${cameraY}px`;
-    canvas.style.right = `${cameraX}px`;
+  canvas.style.left = "";
+  let draggedX = event.clientX - dragstartX;
+  let draggedY = event.clientY - dragstartY;
+  dragstartX = event.clientX;
+  dragstartY = event.clientY;
+  cameraX -= draggedX;
+  cameraY += draggedY;
+  canvas.style.top = `${cameraY}px`;
+  canvas.style.right = `${cameraX}px`;
 }
 
 function onMouseUp(event) {
-    dragstartX = event.clientX;
-    dragstartY = event.clientY;
-    document.removeEventListener("mousemove", onMouseMove);
-    document.removeEventListener("mouseup", onMouseUp);
+  dragstartX = event.clientX;
+  dragstartY = event.clientY;
+  document.removeEventListener("mousemove", onMouseMove);
+  document.removeEventListener("mouseup", onMouseUp);
 }
 
 canvas.addEventListener("mousedown", (event) => {
-    if (event.button != 0 && event.button != 2) {
-        return;
+  if (event.button != 0 && event.button != 2) {
+    return;
+  }
+  let canvasRect = canvas.getBoundingClientRect();
+  let x = (event.clientX - canvasRect.left) / zoom;
+  let y = (event.clientY - canvasRect.top) / zoom;
+  let tileX = Math.round(x / cellSize - 0.5) - Math.round(halfGridWidth * 2);
+  let tileY = Math.round(y / cellSize - 0.5) - Math.round(halfGridHeight * 2);
+  let cell = {};
+  cell["loc_col"] = tileY;
+  cell["loc_row"] = tileX;
+  cell["state"] = {};
+  if (event.button == 0) {
+    cell["state"] = { name: currentCellType };
+    organism.anatomy.cells.push(cell);
+  } else if (event.button == 2) {
+    for (let i = 0; i < organism.anatomy.cells.length; i++) {
+      let c = organism.anatomy.cells[i];
+      if (c.loc_col == tileY && c.loc_row == tileX) {
+        organism.anatomy.cells.splice(i, 1);
+        break;
+      }
     }
-    let canvasRect = canvas.getBoundingClientRect();
-    let x = event.clientX - canvasRect.left;
-    let y = event.clientY - canvasRect.top;
-    let tileX = Math.round(x / cellSize - 0.5) - Math.round(halfGridWidth * 2);
-    let tileY = Math.round(y / cellSize - 0.5) - Math.round(halfGridHeight * 2);
-    let cell = {};
-    cell["loc_col"] = tileY;
-    cell["loc_row"] = tileX;
-    cell["state"] = {};
-    if (event.button == 0) {
-        cell["state"] = {name: currentCellType};
-        organism.anatomy.cells.push(cell);
-    } else if (event.button == 2) {
-        for (i = 0; i < organism.anatomy.cells.length; i++) {
-            let c = organism.anatomy.cells[i];
-            if (c.loc_col == tileY && c.loc_row == tileX) {
-                organism.anatomy.cells.splice(i, 1);
-                break;
-            }
-        }
-    }
-    drawCells();
-    updateGraph();
+  }
+  drawCells();
+  updateGraph();
 });
 
 function drawCells() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = colors.gray;
-    let middleX = Math.floor(canvas.width / (2 * cellSize));
-    let middleY = Math.floor(canvas.height / (2 * cellSize));
-    ctx.fillRect(middleX * cellSize, middleY * cellSize, cellSize, cellSize);
+  ctx.fillStyle = colors.gray;
+  let middleX = Math.floor(canvas.width / (2 * cellSize));
+  let middleY = Math.floor(canvas.height / (2 * cellSize));
+  ctx.fillRect(middleX * cellSize, middleY * cellSize, cellSize, cellSize);
 
-    let cellLength = organism.anatomy.cells.length;
-    for (let index = 0; index < cellLength; index++) {
-        let cell = organism.anatomy.cells[index];
-        let colorName = cellNames[cell.state.name];
-        ctx.fillStyle = colors[colorName];
-        let cellX = cell.loc_row * cellSize + halfGridWidth * 2 * cellSize; // - cameraX
-        let cellY = cell.loc_col * cellSize + halfGridHeight * 2 * cellSize; // - cameraY
-        ctx.fillRect(cellX, cellY, cellSize, cellSize);
-    }
+  let cellLength = organism.anatomy.cells.length;
+  for (let index = 0; index < cellLength; index++) {
+    let cell = organism.anatomy.cells[index];
+    let colorName = cellNames[cell.state.name];
+    ctx.fillStyle = colors[colorName];
+    let cellX = cell.loc_row * cellSize + halfGridWidth * 2 * cellSize; // - cameraX
+    let cellY = cell.loc_col * cellSize + halfGridHeight * 2 * cellSize; // - cameraY
+    ctx.fillRect(cellX, cellY, cellSize, cellSize);
+  }
 
-    ctx.fillStyle = "#555";
-    ctx.beginPath();
-    ctx.arc(
-        (middleX * cellSize) + cellSize / 2,
-        (middleY * cellSize) + cellSize / 2,
-        cellSize * 0.3,
-        0,
-        2 * Math.PI
-    );
-    ctx.fill();
-    ctx.closePath();
+  ctx.fillStyle = "#555";
+  ctx.beginPath();
+  ctx.arc(
+    middleX * cellSize + cellSize / 2,
+    middleY * cellSize + cellSize / 2,
+    cellSize * 0.3,
+    0,
+    2 * Math.PI
+  );
+  ctx.fill();
+  ctx.closePath();
 }
 
 function updateGraph() {
-    let top_corners = [];
-    let bottom_corners = [];
-    let left_corners = [];
-    let right_corners = [];
-    let startX = 0; // canvas.width % cellSize
-    for (let x = startX; x < canvas.width; x += cellSize) {
-        top_corners.push([x, 0]);
-        bottom_corners.push([x, canvas.height]);
-    }
-    let startY = 0; // canvas.height % cellSize
-    for (let y = startY; y < canvas.height; y += cellSize) {
-        left_corners.push([0, y]);
-        right_corners.push([canvas.width, y]);
-    }
-    ctx.strokeStyle = colors["black"]; // ctx.fillStyle
-    ctx.beginPath();
-    for (let index = 0; index < top_corners.length; index += 1) {
-        let coord1;
-        let coord2;
-        coord1 = top_corners[index];
-        coord2 = bottom_corners[index];
-        ctx.moveTo(coord1[0], coord1[1]);
-        ctx.lineTo(coord2[0], coord2[1]);
-        ctx.stroke();
-    }
-    for (let index = 0; index < left_corners.length; index += 1) {
-        let coord1;
-        let coord2;
-        coord1 = left_corners[index];
-        coord2 = right_corners[index];
-        ctx.moveTo(coord1[0], coord1[1]);
-        ctx.lineTo(coord2[0], coord2[1]);
-        ctx.stroke();
-    }
-    ctx.closePath();
+  let top_corners = [];
+  let bottom_corners = [];
+  let left_corners = [];
+  let right_corners = [];
+  let startX = 0; // canvas.width % cellSize
+  for (let x = startX; x < canvas.width; x += cellSize) {
+    top_corners.push([x, 0]);
+    bottom_corners.push([x, canvas.height]);
+  }
+  let startY = 0; // canvas.height % cellSize
+  for (let y = startY; y < canvas.height; y += cellSize) {
+    left_corners.push([0, y]);
+    right_corners.push([canvas.width, y]);
+  }
+  ctx.strokeStyle = colors["black"]; // ctx.fillStyle
+  ctx.beginPath();
+  for (let index = 0; index < top_corners.length; index += 1) {
+    let coord1;
+    let coord2;
+    coord1 = top_corners[index];
+    coord2 = bottom_corners[index];
+    ctx.moveTo(coord1[0], coord1[1]);
+    ctx.lineTo(coord2[0], coord2[1]);
+    ctx.stroke();
+  }
+  for (let index = 0; index < left_corners.length; index += 1) {
+    let coord1;
+    let coord2;
+    coord1 = left_corners[index];
+    coord2 = right_corners[index];
+    ctx.moveTo(coord1[0], coord1[1]);
+    ctx.lineTo(coord2[0], coord2[1]);
+    ctx.stroke();
+  }
+  ctx.closePath();
 }
 
-document.addEventListener('DOMContentLoaded', (event) => resizeCanvas(event), drawCells(), updateGraph());
+document.addEventListener(
+  "DOMContentLoaded",
+  (event) => resizeCanvas(event),
+  drawCells(),
+  updateGraph()
+);
