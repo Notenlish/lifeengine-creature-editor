38842c806553358864f573dd5f3b73da627a1d9b
Notenlish <ihsancarkci1@gmail.com>
2023-05-23 11:30:20 +0300

    Update editor.js

--- src/js/editor.js
+++ src/js/editor.js
@@ -82,7 +82,7 @@ let organism = {
 
 // Functions
 
-document.addEventListener("contextmenu", event => event.preventDefault());
+canvasContainer.addEventListener("contextmenu", event => event.preventDefault());
 
 function flip(org) {
   for (let i = 0; i < org.anatomy.cells.length; i++) {
